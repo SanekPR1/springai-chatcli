8750c73966b509fde4b4bfb4c88c833d8b936bac
Aleksandr Makridin <epam_amakridin@exactsciences.com>
2025-04-06 16:42:39 -0500

    #0000: initial commit

---
+++ src/main/java/com/example/makridin/springai_chatcli/SpringaiChatcliApplication.java
@@ -0,0 +1,13 @@
+package com.example.makridin.springai_chatcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiChatcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiChatcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makridin/springai_chatcli/SpringaiChatcliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makridin.springai_chatcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiChatcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
